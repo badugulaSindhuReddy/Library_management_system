0d881afbf2ce27c1d6baea590b8a4549b29fbe2a
badugulaSindhuReddy <125346173+badugulaSindhuReddy@users.noreply.github.com>
2025-03-06 18:39:47 -0600

    Create DatabaseConnection.java
    database

---
+++ DatabaseConnection.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/library";
+    private static final String USER = "your_username";
+    private static final String PASSWORD = "your_password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
